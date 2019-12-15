436c787ac2b503444ccb8ef4b124880f3bcd3516
Antti Risteli <ristela1@antti-hima.local>
2019-12-15 16:26:28 +0200

    Initial stats collection

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ use byteorder::{BigEndian, ByteOrder, LittleEndian};
 use crossbeam_channel::{bounded, unbounded};
 use crossbeam_utils::thread;
 use memmap::MmapOptions;
-use osm_pbf::{Blob, BlobHeader, PrimitiveBlock};
+use osm_pbf::{Blob, BlobHeader, DenseInfo, DenseNodes, Info, Node, PrimitiveBlock, Relation, Way};
 use quick_protobuf::{BytesReader, MessageRead};
+use std::cmp::{max, min};
 use std::fs::File;
 
 #[global_allocator]
@@ -18,6 +19,15 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 const WORK_BOUND: usize = 4000;
 const MAX_COMPRESSED_BLOB_SIZE: i32 = 64 * 1024;
 const MAX_DECOMPRESSED_BLOB_SIZE: i32 = 32 * 1024 * 1024;
+
+#[derive(Debug)]
+struct OsmStats {
+    timestamp_min: i64,
+    timestamp_max: i64,
+    nodes: u64,
+    ways: u64,
+    relations: u64,
+}
 
 fn main() {
     let args: Vec<_> = std::env::args_os().collect();
@@ -39,7 +49,7 @@ fn do_processing(filename: &std::ffi::OsStr, thread_count: usize) -> Result<Stri
     let mut reader = BytesReader::from_bytes(&bytes);
     let mut sent_messages = 0;
     let (sender, receiver) = bounded::<Blob>(WORK_BOUND);
-    let (return_sender, return_received) = unbounded::<u64>();
+    let (return_sender, return_received) = unbounded::<OsmStats>();
 
     thread::scope(|s| {
         for _ in 0..thread_count {
@@ -47,18 +57,18 @@ fn do_processing(filename: &std::ffi::OsStr, thread_count: usize) -> Result<Stri
             let cloned_return_sender = return_sender.clone();
             s.spawn(move |_| {
                 let mut buffer = Vec::with_capacity(MAX_DECOMPRESSED_BLOB_SIZE as usize);
-                let mut size_sum = 0 as u64;
+                let mut stats = empty_osm_stats();
                 loop {
                     match cloned_receiver.recv() {
                         Ok(blob) => {
-                            size_sum += handle_block(&blob, &mut buffer) as u64;
+                            handle_block(&mut stats, &blob, &mut buffer);
                             buffer.clear();
                         }
                         Err(_e) => break,
                     }
                 }
                 cloned_return_sender
-                    .send(size_sum)
+                    .send(stats)
                     .expect("failed to send size result");
             });
         }
@@ -97,17 +107,22 @@ fn do_processing(filename: &std::ffi::OsStr, thread_count: usize) -> Result<Stri
         drop(sender);
 
         let mut received_messages = 0;
-        let mut sizes = 0;
+        let mut osm_stats = empty_osm_stats();
         while received_messages < thread_count {
-            sizes += return_received.recv().unwrap();
+            let worker_stats = return_received.recv().unwrap();
+            osm_stats.nodes += worker_stats.nodes;
+            osm_stats.ways += worker_stats.ways;
+            osm_stats.relations += worker_stats.relations;
+            osm_stats.timestamp_max = max(osm_stats.timestamp_max, worker_stats.timestamp_max);
+            osm_stats.timestamp_min = min(osm_stats.timestamp_min, worker_stats.timestamp_min);
             received_messages += 1;
         }
-        Ok(format!("jeee string tables total size {}", sizes))
+        Ok(format!("{:#?}", osm_stats))
     })
     .unwrap()
 }
 
-fn handle_block(blob: &Blob, buffer: &mut Vec<u8>) -> usize {
+fn handle_block(mut osm_stats: &mut OsmStats, blob: &Blob, buffer: &mut Vec<u8>) {
     let zlib_data_ref = blob.zlib_data.as_ref();
     let tried_block = if blob.raw.is_some() {
         let bytes = blob.raw.as_ref().unwrap();
@@ -121,12 +136,6 @@ fn handle_block(blob: &Blob, buffer: &mut Vec<u8>) -> usize {
 
         let mut decompress = Decompress::new(true);
 
-        /*let r = io::Cursor::new(zlib_data_ref.unwrap());
-        let mut zr = ZlibDecoder::new(r);
-
-        zr.read_to_end(buffer)
-            .expect("failed to read to end gzipped"); */
-
         decompress
             .decompress_vec(&zlib_data_ref.unwrap(), buffer, FlushDecompress::Finish)
             .expect("error decompressing");
@@ -139,22 +148,63 @@ fn handle_block(blob: &Blob, buffer: &mut Vec<u8>) -> usize {
         None
     };
     let block = tried_block.unwrap();
-    block.stringtable.s.len()
+    handle_primitive_block(&mut osm_stats, &block);
 }
-/*
-fn parse_block(blob: &Blob) -> Result<&PrimitiveBlock, String> {
-    if blob.raw.is_some() {
-        let bytes = &blob.raw.unwrap();
-        let mut reader = BytesReader::from_bytes(bytes);
-        &PrimitiveBlock::from_reader(&mut reader, bytes)
-    /*} else if blob.zlib_data.is_some() {
-    use flate2::read::ZlibDecoder;
-    let r = io::Cursor::new(blob.zlib_data.unwrap());
-    let mut zr = ZlibDecoder::new(r);
-    let mut reader = BytesReader::from_reader(zr);
-    PrimitiveBlock::from_reader(reader, zr)*/
-    } else {
-        Err("unsupported data")
+
+fn handle_primitive_block(mut osm_stats: &mut OsmStats, block: &PrimitiveBlock) {
+    let date_granularity = block.date_granularity as i64;
+    for primitive in &block.primitivegroup {
+        for node in &primitive.nodes {
+            handle_node(&mut osm_stats, &node, date_granularity);
+        }
+        for way in &primitive.ways {
+            handle_way(&mut osm_stats, &way, date_granularity);
+        }
+        for relation in &primitive.relations {
+            handle_relation(&mut osm_stats, &relation, date_granularity);
+        }
     }
 }
-*/
+
+fn handle_node(mut osm_stats: &mut OsmStats, node: &Node, date_granularity: i64) {
+    osm_stats.nodes += 1;
+    if let Some(info) = &node.info {
+        handle_info(&mut osm_stats, &info, date_granularity)
+    }
+}
+
+fn handle_way(mut osm_stats: &mut OsmStats, way: &Way, date_granularity: i64) {
+    osm_stats.ways += 1;
+    if let Some(info) = &way.info {
+        handle_info(&mut osm_stats, &info, date_granularity)
+    }
+}
+
+fn handle_relation(mut osm_stats: &mut OsmStats, relation: &Relation, date_granularity: i64) {
+    osm_stats.relations += 1;
+    if let Some(info) = &relation.info {
+        handle_info(&mut osm_stats, &info, date_granularity)
+    }
+}
+
+fn handle_info(osm_stats: &mut OsmStats, info: &Info, date_granularity: i64) {
+    if let Some(timestamp) = info.timestamp {
+        let millisec_stamp = timestamp * date_granularity;
+        if millisec_stamp < osm_stats.timestamp_min {
+            osm_stats.timestamp_min = millisec_stamp
+        }
+        if millisec_stamp > osm_stats.timestamp_max {
+            osm_stats.timestamp_max = millisec_stamp
+        }
+    }
+}
+
+fn empty_osm_stats() -> OsmStats {
+    OsmStats {
+        nodes: 0,
+        relations: 0,
+        timestamp_max: std::i64::MIN,
+        timestamp_min: std::i64::MAX,
+        ways: 0,
+    }
+}
